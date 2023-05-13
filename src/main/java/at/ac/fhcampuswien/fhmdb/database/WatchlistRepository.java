45f7d39cf95730fa523f18ba07375ec502a50728
Fries Aaron-Dario <113194397+Aaron4732@users.noreply.github.com>
2023-05-13 17:38:59 +0200

    add funktions in WhatchlistRepostiory: - initialicer - ContactRepository - addMovie - removeMovie - getMovies

--- src/main/java/at/ac/fhcampuswien/fhmdb/database/WatchlistRepository.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/database/WatchlistRepository.java
@@ -1,5 +1,38 @@
 package at.ac.fhcampuswien.fhmdb.database;
 
+import at.ac.fhcampuswien.fhmdb.models.Movie;
+import com.j256.ormlite.dao.Dao;
+
+import java.sql.SQLException;
+
 public class WatchlistRepository {
+    Dao<WatchlistMovieEntity, Long> dao;
+
+    public void ContactRepository() {
+        this.dao = Database.getDatabese().getDao();
+    }
+
+    public void addMovie(Movie movie) throws SQLException {
+        WatchlistMovieEntity movieToAdd = new WatchlistMovieEntity(
+                movie.getTitle(),
+                movie.getDescription(),
+                movie.getGenresAssString(),
+                movie.getReleaseYear(),
+                movie.getImgUrl(),
+                movie.getLengthInMinutes(),
+                movie.getRating()
+        );
+
+        dao.create(movieToAdd);
+    }
+
+    public void removeMovie() {
+        return;
+    }
+
+    public Movie[] getMovies() {
+        return new Movie[]{};
+    }
+
 
 }
