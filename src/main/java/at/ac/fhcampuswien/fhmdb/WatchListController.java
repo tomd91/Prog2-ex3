4b760942e98e49b18c1295eb69d6a3762e59e260
tom <tom.doberauer@stud.fh-campuswien.ac.at>
2023-05-14 15:38:45 +0200

    formatted watchlist-view.fxml / added watchlist Btn and logic / edited detail Btn to accommodate for altered layout

--- src/main/java/at/ac/fhcampuswien/fhmdb/WatchListController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/WatchListController.java
@@ -1,6 +1,12 @@
 package at.ac.fhcampuswien.fhmdb;
 
+import com.jfoenix.controls.JFXListView;
+import javafx.fxml.FXML;
+
 public class WatchListController {
+
+    @FXML
+    public JFXListView movieListView;
 
     public void initialize(){
         System.out.println("Watchlist Controller initialized!");
